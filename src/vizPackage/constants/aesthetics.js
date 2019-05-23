66720020a3c663a89aa83f172ddd99b88ec8f174
Alice Ma <alice.ma@enigma.com>
2019-05-23 13:18:29 -0400

    FEATURE: fade out line and points on hover

---
+++ src/vizPackage/constants/aesthetics.js
@@ -0,0 +1,9 @@
+export const MARGIN_LEFT = 30;
+export const MARGIN_TOP = 30;
+
+export const CHART_HEIGHT = 300;
+export const CHART_WIDTH = 300;
+
+export const TURQUOISE = '#70cbce';
+export const POINT_RADIUS = 5;
+export const LINE_WIDTH = 3;
--- src/vizPackage/index.js
+++ src/vizPackage/index.js
@@ -1,5 +1,6 @@
 import * as d3 from 'd3';
 
+import * as aesthetics from './constants/aesthetics';
 import './style.css';
 
 // import disney_data from './data/disney_payload.json';
@@ -21,48 +22,79 @@ class VizPackage {
 			.attr('height', this.height);
 
 		this.container = this.svg.append('g')
-			.attr('transform', 'translate(' + 30 + ',' + 30 + ')');
+			.attr('transform', 'translate(' + aesthetics.MARGIN_LEFT + ',' + aesthetics.MARGIN_TOP + ')');
 
 		this.xScale = d3.scaleLinear()
-			.domain([0, 70])
-			.range([0, 600]);
+			.domain([0, 50])
+			.range([0, aesthetics.CHART_WIDTH]);
 
 		this.yScale = d3.scaleLinear()
-			.domain([0, 200])
-			.range([200, 0]);
+			.domain([0, 50])
+			.range([aesthetics.CHART_HEIGHT, 0]);
 
 		this.generateLine = d3.line()
 			.x((d) => { return this.xScale(d.age); })
 			.y((d) => { return this.yScale(d.tenure); })
 			.curve(d3.curveMonotoneX);
 
-		this.line = this.container.append('path')
-			.data([data])
-			.attr('fill', 'none')
-			.attr('stroke', '#70cbce')
-			.attr('stroke-width', 3)
-			.attr('d', this.generateLine);
-
 		this.xAxis = this.container.append('g')
 			.attr('class', 'x axis')
-			.attr('transform', 'translate(0,' + 200 + ')')
+			.attr('transform', 'translate(0,' + aesthetics.CHART_HEIGHT + ')')
 			.call(d3.axisBottom(this.xScale));
 
 		this.yAxis = this.container.append('g')
 			.attr('class', 'y axis')
 			.call(d3.axisLeft(this.yScale));
 
+		this.line = this.container.append('path');
 		this.point = this.container.append('g').selectAll('.point');
+		this.update();
+	}
+
+	update = () => {
+		const self = this;
+
+		this.line = this.line.data([data])
+			.attr('class', 'line')
+			.attr('fill', 'none')
+			.attr('stroke', aesthetics.TURQUOISE)
+			.attr('stroke-width', aesthetics.LINE_WIDTH)
+			.attr('d', this.generateLine);
+
 		this.point = this.point.data(data);
 
-		this.pointsEnter = this.point
-			.enter()
+		this.pointEnter = this.point.enter()
 			.append('circle')
 			.attr('class', 'point')
-			.attr('r', 5)
+			.attr('r', aesthetics.POINT_RADIUS)
 			.attr('cx', (d) => { return this.xScale(d.age); })
-			.attr('cy', (d) => { return this.yScale(d.tenure); });
+			.attr('cy', (d) => { return this.yScale(d.tenure); })
+			.on('mouseenter', function(d) { self.mouseenter(d, this); })
+			.on('mouseleave', function(d) { self.mouseleave(d, this); });
+	
+		this.point.exit().remove();
+		this.point = this.pointEnter.merge(this.point);
 	}
+
+	mouseenter = (d, self) => {
+		const point = d3.select(self);
+		this.fadeNodes(d);
+		this.fadeLinks(d);
+	}
+
+	mouseleave = (d, self) => {
+		this.point.classed('faded', false);
+		this.line.classed('faded', false);
+	}
+
+	fadeNodes = (d) => {
+		this.point.classed('faded', (n) => { return n.id !== d.id; });
+	}
+
+	fadeLinks = (d) => {
+  	this.line.classed('faded', (l) => { return true; });
+	}
+
 }
 
 export default VizPackage;
